2b3fda33e267b75f56d9ab3dab8ff4b8f76aeadc
lucalavazza <l.lavazza@studenti.unibs.it>
2025-10-02 14:59:47 +0200

    GRaSP and Granger added

--- causal-learn_analysis.py
+++ causal-learn_analysis.py
@@ -9,6 +9,9 @@ from causallearn.search.ConstraintBased.PC import pc
 from causallearn.search.ConstraintBased.FCI import fci
 from causallearn.search.ScoreBased.GES import ges
 from causallearn.search.FCMBased import lingam
+from causallearn.search.HiddenCausal.GIN.GIN import GIN
+from causallearn.search.PermutationBased.GRaSP import grasp
+from causallearn.search.Granger.Granger import Granger
 import time
 
 np.random.seed(42)
@@ -115,11 +118,10 @@ for cit in cits:
 
 # Score-based Method
 # GES
-if data_type != 'encoded':  # TODO: does it only take a LOOOOONG time, or does it not work? To find out.
-    score_funcs = ['local_score_BIC', 'local_score_BDeu', 'local_score_marginal_general']
+if data_type != 'encoded':  # TODO: does this it only take a LOOOOONG time, or does it not work? To find out.
+    score_funcs = ['local_score_BIC', 'local_score_BDeu']
     for sf in score_funcs:
-        if sf not in ['local_score_BDeu',
-                      'local_score_marginal_general']:  # TODO: do they only take a LOOOOONG time, or do they not work? To find out.
+        if sf not in ['local_score_BDeu']:  # TODO: does this only take a LOOOOONG time, or does this not work? To find out.
             print('---> GES, score function = ' + str(sf))
             start_pc = time.time()
             cg_ges = ges(fit_data, score_func=sf)
@@ -154,7 +156,7 @@ if data_type != 'encoded':  # TODO: does it only take a LOOOOONG time, or does i
                 np_ges_edges_names.append(new_edge)
             for node in np_ges_nodes:
                 np_ges_nodes_names.append(var_names[node])
-            np_pc_edges_names = np.array(np_ges_edges_names)
+            np_ges_edges_names = np.array(np_ges_edges_names)
 
             if data_type == 'encoded':
                 np.save(npy_dir + '/encoding_causal_graph_causal-learn_ges_' + str(sf) + '.npy', np_ges_edges_names)
@@ -227,4 +229,131 @@ if data_type != 'encoded':  # TODO: does it only take a LOOOOONG time, or does i
         with open(txt_dir + '/labelling_causal_graph_causal-learn_lingam.txt', 'w') as f:
             for edge in np_lin_edges_names:
                 f.write(f"{edge}\n")
+
+
+# Hidden Causal Representation Learning
+# GIN
+if data_type not in ['labelled', 'encoded']:  # TODO: does it only take a LOOOOONG time, or does it not work? To find out.
+    print('---> GIN')
+    start_pc = time.time()
+    cg_gin, order_gin = GIN(fit_data)
+    print('GIN: ' + str((time.time() - start_pc)) + " seconds\n\n")
+    pcg_gin = GraphUtils.to_pydot(cg_gin.G, labels=var_names)
+    if data_type == 'encoded':
+        pcg_gin.write_png(
+            graphs_dir + '/onehot/GIN-onehot/encoding_causal_graph_causal-learn_gin.png')
+    else:
+        pcg_gin.write_png(graphs_dir + '/labelling/GIN-labelling/labelling_causal_graph_causal-learn_gin.png')
+
+
+# Permutation-based Methods
+# GRaSP
+score_funcs = ['local_score_BIC', 'local_score_BDeu']
+for sf in score_funcs:
+    if sf not in ['local_score_BDeu']:  # TODO: does this only take a LOOOOONG time, or does this not work? To find out.
+        print('---> GRaSP with score_func = ' + str(sf))
+        start_pc = time.time()
+        cg_grasp = grasp(fit_data, sf)
+        print("GRaSP with score_func = " + str(sf) + ": " + str((time.time() - start_pc)) + " seconds\n\n")
+        pcg_grasp = GraphUtils.to_pydot(cg_grasp, labels=var_names)
+        if data_type == 'encoded':
+            pcg_grasp.write_png(
+                graphs_dir + '/onehot/GRaSP-onehot/encoding_causal_graph_causal-learn_grasp_' + str(sf) + '_.png')
+        else:
+            pcg_grasp.write_png(
+                graphs_dir + '/labelling/GRaSP-labelling/labelling_causal_graph_causal-learn_grasp_' + str(sf) + '_.png')
+
+        grasp_edges = cg_grasp.get_graph_edges()
+        np_grasp_edges = []
+        for i in range(len(grasp_edges)):
+            edge = [int(str(grasp_edges[i].get_node1()).replace('X', ''))-1, int(str(grasp_edges[i].get_node2()).replace('X', ''))-1]
+            np_grasp_edges.append(np.array(edge))
+
+        np_grasp_nodes = []
+        for edge in np_grasp_edges:
+            for node in edge:
+                if node not in np_grasp_nodes:
+                    np_grasp_nodes.append(node)
+        np_grasp_names = var_names.to_numpy()
+        np_grasp_edges_names = []
+        np_grasp_nodes_names = []
+        for edge in np_grasp_edges:
+            new_edge = []
+            for node in edge:
+                new_node = var_names[node]
+                new_edge.append(new_node)
+            np_grasp_edges_names.append(new_edge)
+        for node in np_grasp_nodes:
+            np_grasp_nodes_names.append(var_names[node])
+        np_grasp_edges_names = np.array(np_grasp_edges_names)
+
+        if data_type == 'encoded':
+            np.save(npy_dir + '/encoding_causal_graph_causal-learn_grasp_' + str(sf) + '.npy', np_grasp_edges_names)
+            with open(txt_dir + '/encoding_causal_graph_causal-learn_grasp_' + str(sf) + '.txt', 'w') as f:
+                for edge in np_grasp_edges_names:
+                    f.write(f"{edge}\n")
+        else:
+            np.save(npy_dir + '/labelling_causal_graph_causal-learn_grasp_' + str(sf) + '.npy', np_grasp_edges_names)
+            with open(txt_dir + '/labelling_causal_graph_causal-learn_grasp_' + str(sf) + '.txt', 'w') as f:
+                for edge in np_grasp_edges_names:
+                    f.write(f"{edge}\n")
+
+
+# Granger Causality
+print('---> Granger')
+start_pc = time.time()
+G = Granger()
+coeff = G.granger_lasso(fit_data)
+print('Granger: ' + str((time.time() - start_pc)) + " seconds\n\n")
+
+num_nodes = coeff.shape[0]
+cg_granger = CausalGraph(num_nodes)
+for i in range(num_nodes):
+    for j in range(num_nodes):
+        edge1 = cg_granger.G.get_edge(cg_granger.G.nodes[i], cg_granger.G.nodes[j])
+        if edge1 is not None:
+            cg_granger.G.remove_edge(edge1)
+
+for i in range(num_nodes):
+    for j in range(num_nodes):
+        if coeff[i, j] > 0:
+            cg_granger.G.add_edge(Edge(cg_granger.G.nodes[i], cg_granger.G.nodes[j], Endpoint.TAIL, Endpoint.ARROW))
+        elif coeff[i, j] < 0:
+            cg_granger.G.add_edge(Edge(cg_granger.G.nodes[j], cg_granger.G.nodes[i], Endpoint.TAIL, Endpoint.ARROW))
+
+pcg_granger = GraphUtils.to_pydot(cg_granger.G, labels=var_names)
+if data_type == 'encoded':
+    pcg_granger.write_png(graphs_dir + '/onehot/Granger-onehot/encoding_causal_graph_causal-learn_granger.png')
+else:
+    pcg_granger.write_png(graphs_dir + '/labelling/Granger-labelling/labelling_causal_graph_causal-learn_granger.png')
+
+np_granger_edges = np.array(cg_granger.find_fully_directed())
+np_granger_nodes = []
+for edge in np_granger_edges:
+    for node in edge:
+        if node not in np_granger_nodes:
+            np_granger_nodes.append(node)
+np_granger_names = var_names.to_numpy()
+np_granger_edges_names = []
+np_granger_nodes_names = []
+for edge in np_granger_edges:
+    new_edge = []
+    for node in edge:
+        new_node = var_names[node]
+        new_edge.append(new_node)
+    np_granger_edges_names.append(new_edge)
+for node in np_granger_nodes:
+    np_granger_nodes_names.append(var_names[node])
+np_granger_edges_names = np.array(np_granger_edges_names)
+
+if data_type == 'encoded':
+    np.save(npy_dir + '/encoding_causal_graph_causal-learn_granger.npy', np_granger_edges_names)
+    with open(txt_dir + '/encoding_causal_graph_causal-learn_granger.txt', 'w') as f:
+        for edge in np_granger_edges_names:
+            f.write(f"{edge}\n")
+else:
+    np.save(npy_dir + '/labelling_causal_graph_causal-learn_lingam.npy', np_granger_edges_names)
+    with open(txt_dir + '/labelling_causal_graph_causal-learn_lingam.txt', 'w') as f:
+        for edge in np_granger_edges_names:
+            f.write(f"{edge}\n")
 
